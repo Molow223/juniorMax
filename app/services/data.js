b4e070a11e6f25ba54e254048af206759f5b1cad
Molow223 <molow223@gmail.com>
2021-12-17 22:11:44 +0500

    17.12

--- app/services/data.js
+++ app/services/data.js
@@ -51,7 +51,7 @@ export default Service.extend({
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(speaker),
+      body: JSON.stringify(speaker)
     });
   },
-});
+}); 
